2e7782fe9565423fc6c292bd0eb39ebcc62cf56d
Rito Eduardo García Lara <riedgala@gmail.com>
2019-08-19 14:52:34 -0500

    Mejoras y descripción

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # to-do
 
+Instalar dependencias:
+
+```
 npm install
+```
+
+Probar el programa:
+
+```
+node app --help
+```
+
+Ejemplos:
+
+```
+node app create -d "Pasear al perro"
+```
--- app.js
+++ app.js
@@ -5,31 +5,31 @@ const color = require('colors');
 
 switch (argv._[0]) {
     case 'create':
-        var resp = todo.create( argv.description , argv.completed === "false" ? false : argv.completed, Number( argv.id ) );
-        console.log( resp );    
+        var resp = todo.create(argv.description, argv.completed === "false" ? false : argv.completed, Number(argv.id));
+        console.log(resp);
         break;
     case 'read':
-        var resp = todo.read( argv.id );
-        console.log( resp );    
+        var resp = todo.read(argv.id);
+        console.log(resp);
         break;
     case 'update':
-        var resp = todo.update( argv.id , argv.description , argv.completed === "false" ? false : argv.completed );
-        console.log( resp );
+        var resp = todo.update(argv.id, argv.description, argv.completed === "false" ? false : argv.completed);
+        console.log(resp);
         break;
     case 'delete':
-        var resp = todo.deleteTask( argv.id );
-        console.log( resp );    
+        var resp = todo.deleteTask(argv.id);
+        console.log(resp);
         break;
     case 'show':
-        let list = todo.getList( argv.completed );   
-        for( let task of list ){
+        let list = todo.getList(argv.completed);
+        for (let task of list) {
             console.log(" ================  TASK ============== ".green);
-            console.log(" Description : " + task.description );
-            console.log(" Completed : "+ task.completed );
-            console.log(" Id : "+ task.id );
+            console.log(" Description : " + task.description);
+            console.log(" Completed : " + task.completed);
+            console.log(" Id : " + task.id);
         }
         break;
     default:
-        console.log("El comando no es reconocido");    
+        console.log("El comando no es reconocido");
         break;
 }
